14a4bdad895bc6bab869a6d9f740de2503f3e3cd
liting <luz.liting@gmail.com>
2024-05-02 11:37:26 +0800

    chore(editor): init

---
+++ src/components/editor/index.tsx
@@ -0,0 +1,17 @@
+import { useEditor, EditorContent } from '@tiptap/react'
+import starterKit from '@tiptap/starter-kit'
+import { uniqueId } from '@note-editor/tiptap-extension-unique-id'
+import './index.scss'
+
+export default () => {
+  const editor = useEditor({
+    extensions: [
+      starterKit.configure({
+        hardBreak: false
+      }),
+      uniqueId
+    ]
+  })
+
+  return (<EditorContent className='h-full bg-slate-50 m-0 p-4' editor={editor}></EditorContent>)
+}
